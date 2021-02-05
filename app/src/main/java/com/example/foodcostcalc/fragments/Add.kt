eb77cc77a2d5f936a2ad4a35ae70c2e433cc74b7
michalguspiel <guspielmichal@gmail.com>
2021-02-05 15:36:25 +0200

    Major graphic and logic update, added side navigation drawers.

--- app/src/main/java/com/example/foodcostcalc/fragments/Add.kt
+++ app/src/main/java/com/example/foodcostcalc/fragments/Add.kt
@@ -14,30 +14,16 @@ import com.example.foodcostcalc.model.ProductIncluded
 import com.example.foodcostcalc.fragments.dialogs.CreateDish
 import com.example.foodcostcalc.model.Product
 import com.example.foodcostcalc.viewmodel.AddViewModel
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 
-class Add : Fragment(), AdapterView.OnItemSelectedListener {
-    private val NEW_SPINNER_ID = 1
-    private val ANOTHER_SPINNER_ID = 2
-    private var productPosition: Int? = null
-    private var dishPosition: Int? = null
+class Add : Fragment() {
+
+
 
     private fun showToast(context: FragmentActivity? = activity, message: String, duration: Int = Toast.LENGTH_LONG) {
         Toast.makeText(context, message, duration) .show()
-    }
-    /**Spinner implementation TODO */
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        showToast(activity,"nothing selected ",3)
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        when (parent?.id) {
-            1 -> {productPosition = position
-            }
-            else -> {dishPosition = position
-            }
-        }
     }
 
     override fun onCreateView(
@@ -54,74 +40,16 @@ class Add : Fragment(), AdapterView.OnItemSelectedListener {
             val price                = view.findViewById<EditText>(R.id.product_price)
             val tax                  = view.findViewById<EditText>(R.id.product_tax)
             val waste                = view.findViewById<EditText>(R.id.product_waste)
-            val weightOfAddedProduct = view.findViewById<EditText>(R.id.product_weight)
             val addButton            = view.findViewById<Button>(R.id.addProduct)
-            val createDishButton     = view.findViewById<Button>(R.id.new_dish_button)
-            val addProductToDishBtn  = view.findViewById<ImageButton>(R.id.add_product_to_dish)
             val calculateWasteBtn    = view.findViewById<Button>(R.id.count_waste_percent_btn)
             val calcWasteWeight      = view.findViewById<EditText>(R.id.waste_calc_product_waste)
             val calcProductWeight    = view.findViewById<EditText>(R.id.waste_calc_product_weight)
 
 
-            /** ADAPTERs FOR SPINNERs */
-            val productList = mutableListOf<String>()
-            viewModel.readAllProductData.observe(viewLifecycleOwner, Observer { it.forEach{product -> productList.add(product.name) } })
-            val productAdapter = ArrayAdapter(requireActivity(),R.layout.spinner_layout,productList)
-        productAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-             val mySpinner = view.findViewById<Spinner>(R.id.mySpinner)
-            with(mySpinner)
-            {
-                adapter = productAdapter
-                setSelection(0, false)
-                onItemSelectedListener = this@Add
-                prompt = "Select product"
-                gravity = Gravity.CENTER
-            }
-            mySpinner.id = NEW_SPINNER_ID
-            productAdapter.notifyDataSetChanged()
-
-            val dishList = mutableListOf<String>()
-            viewModel.readAllDishData.observe(viewLifecycleOwner, Observer { it.forEach{dish -> dishList.add(dish.name)} })
-            val dishesAdapter = ArrayAdapter(requireActivity(),R.layout.spinner_layout,dishList)
-        dishesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            val dishSpinner = view.findViewById<Spinner>(R.id.dishSpinner)
-            with(dishSpinner){
-                adapter = dishesAdapter
-                setSelection(0,false)
-                onItemSelectedListener = this@Add
-                prompt = "Select dish"
-                gravity = Gravity.CENTER
-            }
-            dishSpinner.id =  ANOTHER_SPINNER_ID
-        dishesAdapter.notifyDataSetChanged()
-
-
-            /**OBSERVING LIVEDATA FROM ADDVIEWMODEL
-             *  WHICH OBSERVES LIVEDATA IN REPOSITORY
-             *  WHICH OBSERVES LIVEDATA FROM DAO*/
-
-            viewModel.readAllProductData.observe(viewLifecycleOwner, Observer { products ->
-                productAdapter.clear()
-                products.forEach { product ->
-                    productAdapter.add(product.name)
-                    productAdapter.notifyDataSetChanged()
-                }
-            })
-            viewModel.readAllDishData.observe(viewLifecycleOwner, Observer { dishes ->
-                dishesAdapter.clear()
-                dishes.forEach{dish ->
-                    dishesAdapter.add(dish.name)
-                    dishesAdapter.notifyDataSetChanged()
-                }
-
-            })
 
             /** BUTTONS FUNCTIONALITY */
 
-            createDishButton.setOnClickListener{
-                CreateDish().show(childFragmentManager,
-                    CreateDish.TAG)
-            }
+
 
             addButton.setOnClickListener{
                 if(name.text.isNullOrEmpty()||
@@ -143,23 +71,17 @@ class Add : Fragment(), AdapterView.OnItemSelectedListener {
                     waste.text.clear()
                 }
             }
-            addProductToDishBtn.setOnClickListener {
-                if (weightOfAddedProduct.text.isNullOrEmpty()) { showToast(message = "You can't add product without weight.")
-                } else {
-                    val chosenDish      = viewModel.readAllDishData.value?.get(dishPosition!!)
-                    val chosenProduct   = viewModel.readAllProductData.value?.get(productPosition!!)
-                    val weight          = weightOfAddedProduct.text.toString().toDouble() / 1000
-                    viewModel.addProductToDish(ProductIncluded(0, chosenProduct!!, chosenDish!!.dishId, chosenProduct.productId, weight))
-                    }
-                    weightOfAddedProduct.text.clear()
-                }
+
 
             calculateWasteBtn.setOnClickListener{
                 if(calcProductWeight.text.isNotEmpty() && calcWasteWeight.text.isNotEmpty()){
                     val calcWeight = calcProductWeight.text.toString().toDouble()
                     val calcWaste = calcWasteWeight.text.toString().toDouble()
                     val result = (100 * calcWaste) / calcWeight
-                    waste.setText(result.toString())
+                    val df = DecimalFormat("#.##")
+                    df.roundingMode = RoundingMode.CEILING
+                    val formatedResult = df.format(result)
+                    waste.setText(formatedResult)
 
                     calcProductWeight.text.clear()
                     calcWasteWeight.text.clear()
