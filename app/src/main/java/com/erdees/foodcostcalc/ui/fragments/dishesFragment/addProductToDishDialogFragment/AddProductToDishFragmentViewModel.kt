ee10f1c03a66a4fdce635ef4b7eb795dbde75ba3
michalguspiel <guspielmichal@gmail.com>
2024-01-31 16:34:46 +0200

    Bug fix.

--- app/src/main/java/com/erdees/foodcostcalc/ui/fragments/dishesFragment/addProductToDishDialogFragment/AddProductToDishFragmentViewModel.kt
+++ app/src/main/java/com/erdees/foodcostcalc/ui/fragments/dishesFragment/addProductToDishDialogFragment/AddProductToDishFragmentViewModel.kt
@@ -25,130 +25,154 @@ import java.util.*
 
 class AddProductToDishFragmentViewModel(application: Application) : AndroidViewModel(application) {
 
-    val readAllHalfProductModelData: LiveData<List<HalfProductModel>>
-    val readAllProductModelData: LiveData<List<ProductModel>>
-    val readAllDishModelData: LiveData<List<DishModel>>
+  val readAllHalfProductModelData: LiveData<List<HalfProductModel>>
+  val readAllProductModelData: LiveData<List<ProductModel>>
+  val readAllDishModelData: LiveData<List<DishModel>>
 
-    private val productRepository: ProductRepository
-    private val halfProductRepository: HalfProductRepository
-    private val dishRepository: DishRepository
-    private val productIncludedRepository: ProductIncludedRepository
-    private val halfProductIncludedInDishRepository: HalfProductIncludedInDishRepository
+  private val productRepository: ProductRepository
+  private val halfProductRepository: HalfProductRepository
+  private val dishRepository: DishRepository
+  private val productIncludedRepository: ProductIncludedRepository
+  private val halfProductIncludedInDishRepository: HalfProductIncludedInDishRepository
 
-    val sharedPreferences = SharedPreferences(application)
+  val sharedPreferences = SharedPreferences(application)
 
-    init {
-        val productDao = AppRoomDataBase.getDatabase(application).productDao()
-        val halfProductDao = AppRoomDataBase.getDatabase(application).halfProductDao()
-        val dishDao = AppRoomDataBase.getDatabase(application).dishDao()
-        val productIncludedDao = AppRoomDataBase.getDatabase(application).productIncludedDao()
-        val halfProductIncludedInDishDao =
-            AppRoomDataBase.getDatabase(application).halfProductIncludedInDishDao()
+  init {
+    val productDao = AppRoomDataBase.getDatabase(application).productDao()
+    val halfProductDao = AppRoomDataBase.getDatabase(application).halfProductDao()
+    val dishDao = AppRoomDataBase.getDatabase(application).dishDao()
+    val productIncludedDao = AppRoomDataBase.getDatabase(application).productIncludedDao()
+    val halfProductIncludedInDishDao =
+      AppRoomDataBase.getDatabase(application).halfProductIncludedInDishDao()
 
 
-        halfProductRepository = HalfProductRepository.getInstance(halfProductDao)
-        productRepository = ProductRepository.getInstance(productDao)
-        dishRepository = DishRepository.getInstance(dishDao)
-        productIncludedRepository = ProductIncludedRepository.getInstance(productIncludedDao)
-        halfProductIncludedInDishRepository =
-            HalfProductIncludedInDishRepository.getInstance(halfProductIncludedInDishDao)
+    halfProductRepository = HalfProductRepository.getInstance(halfProductDao)
+    productRepository = ProductRepository.getInstance(productDao)
+    dishRepository = DishRepository.getInstance(dishDao)
+    productIncludedRepository = ProductIncludedRepository.getInstance(productIncludedDao)
+    halfProductIncludedInDishRepository =
+      HalfProductIncludedInDishRepository.getInstance(halfProductIncludedInDishDao)
 
-        readAllHalfProductModelData = halfProductRepository.readAllData
-        readAllProductModelData = productRepository.readAllData
-        readAllDishModelData = dishRepository.readAllData
+    readAllHalfProductModelData = halfProductRepository.readAllData
+    readAllProductModelData = productRepository.readAllData
+    readAllDishModelData = dishRepository.readAllData
+  }
+
+  private var metricCondition = true
+  private var usaCondition = true
+
+  fun updateUnitsConditions() {
+    metricCondition = sharedPreferences.getValueBoolean(Constants.METRIC, true)
+    usaCondition = sharedPreferences.getValueBoolean(Constants.IMPERIAL, false)
+  }
+
+  private val unitList = arrayListOf<String>() // list for units, to populate spinner
+
+  fun getUnitList(): ArrayList<String> = unitList
+
+  private var chosenUnit: String = ""
+
+  fun chooseUnit(position: Int) {
+    chosenUnit = unitList[position]
+  }
+
+  private var unitType = ""
+
+  fun updateUnitList() {
+    unitList.changeUnitList(
+      unitType,
+      metricCondition,
+      usaCondition
+    )
+    chosenUnit = unitList.first()
+  }
+
+  fun setProductUnitType(position: Int) {
+    unitType = UnitsUtils.getUnitType(
+      readAllProductModelData.value?.getOrNull(position)?.unit
+    )
+  }
+
+  fun setHalfProductUnitType(position: Int) {
+    unitType = UnitsUtils.getUnitType(
+      readAllHalfProductModelData.value?.getOrNull(position)?.halfProductUnit
+    )
+  }
+
+  var productPosition: Int? = null
+  var dishPosition: Int? = null
+
+  fun addToDish(
+    weight: String?,
+    isHalfProductChecked: Boolean
+  ): Result {
+    if ((weight.isNullOrEmpty() || weight == ".")) return Result.FailureWeight
+    if (readAllDishModelData.value.isNullOrEmpty()) return Result.FailureDish
+    if (!isHalfProductChecked && readAllProductModelData.value.isNullOrEmpty()) return Result.FailureProduct
+    if (isHalfProductChecked && readAllHalfProductModelData.value.isNullOrEmpty()) return Result.FailureProduct
+
+    return if (!isHalfProductChecked) {
+      val product = addProductToDish(weight.toDouble())
+      Result.SuccessProduct(product.name)
+    } else {
+      val halfProduct = addHalfProductIncludedInDish(weight.toDouble())
+      Result.SuccessHalfProduct(halfProduct.name)
     }
+  }
 
-    private var metricCondition = true
-    private var usaCondition = true
-
-    fun updateUnitsConditions() {
-        metricCondition = sharedPreferences.getValueBoolean(Constants.METRIC, true)
-        usaCondition = sharedPreferences.getValueBoolean(Constants.IMPERIAL, false)
+  private fun addProductToDish(product: ProductIncluded) {
+    viewModelScope.launch(Dispatchers.IO) {
+      productIncludedRepository.addProductToDish(product)
     }
+  }
 
-    private val unitList = arrayListOf<String>() // list for units, to populate spinner
+  private fun addProductToDish(
+    weight: Double
+  ): ProductModel {
+    val product = readAllProductModelData.value?.get(productPosition!!)
+    val dish = readAllDishModelData.value?.get(dishPosition!!)
+    val productIncluded = ProductIncluded(
+      0,
+      product!!,
+      dish!!.dishId,
+      dish,
+      product.productId,
+      weight,
+      chosenUnit
+    )
+    addProductToDish(productIncluded)
+    return product
+  }
 
-    fun getUnitList(): ArrayList<String> = unitList
-
-    private var chosenUnit: String = ""
-
-    fun chooseUnit(position: Int) {
-        chosenUnit = unitList[position]
+  private fun addHalfProductIncludedInDish(halfProductIncludedInDishModel: HalfProductIncludedInDishModel) {
+    viewModelScope.launch(Dispatchers.IO) {
+      halfProductIncludedInDishRepository
+        .addHalfProductIncludedInDish(halfProductIncludedInDishModel)
     }
+  }
 
-    private var unitType = ""
+  private fun addHalfProductIncludedInDish(weight: Double): HalfProductModel {
+    val chosenDish = readAllDishModelData.value?.get(dishPosition!!)
+    val halfProduct = readAllHalfProductModelData.value?.get(productPosition!!)
+    val halfProductIncludedInDish = HalfProductIncludedInDishModel(
+      0,
+      chosenDish!!,
+      chosenDish.dishId,
+      halfProduct!!,
+      halfProduct.halfProductId,
+      weight,
+      chosenUnit
+    )
+    addHalfProductIncludedInDish(halfProductIncludedInDish)
 
-    fun updateUnitList() {
-        unitList.changeUnitList(
-            unitType,
-            metricCondition,
-            usaCondition
-        )
-        chosenUnit = unitList.first()
-    }
+    return halfProduct
+  }
 
-    fun setProductUnitType(position: Int) {
-        unitType = UnitsUtils.getUnitType(
-            readAllProductModelData.value?.get(position)?.unit
-        )
-    }
-
-    fun setHalfProductUnitType(position: Int) {
-        unitType = UnitsUtils.getUnitType(
-            readAllHalfProductModelData.value?.get(position)?.halfProductUnit
-        )
-    }
-
-    var productPosition: Int? = null
-    var dishPosition: Int? = null
-
-    fun getHalfProducts() = halfProductRepository.readAllData
-
-    private fun addProductToDish(product: ProductIncluded) {
-        viewModelScope.launch(Dispatchers.IO) {
-            productIncludedRepository.addProductToDish(product)
-        }
-    }
-
-    fun addProductToDish(
-        weight: Double
-    ): ProductModel {
-        val product = readAllProductModelData.value?.get(productPosition!!)
-        val dish = readAllDishModelData.value?.get(dishPosition!!)
-        val productIncluded = ProductIncluded(
-            0,
-            product!!,
-            dish!!.dishId,
-            dish,
-            product.productId,
-            weight,
-            chosenUnit
-        )
-        addProductToDish(productIncluded)
-        return product
-    }
-
-    private fun addHalfProductIncludedInDish(halfProductIncludedInDishModel: HalfProductIncludedInDishModel) {
-        viewModelScope.launch(Dispatchers.IO) {
-            halfProductIncludedInDishRepository
-                .addHalfProductIncludedInDish(halfProductIncludedInDishModel)
-        }
-    }
-
-    fun addHalfProductIncludedInDish(weight: Double): HalfProductModel {
-        val chosenDish = readAllDishModelData.value?.get(dishPosition!!)
-        val halfProduct = readAllHalfProductModelData.value?.get(productPosition!!)
-        val halfProductIncludedInDish = HalfProductIncludedInDishModel(
-            0,
-            chosenDish!!,
-            chosenDish.dishId,
-            halfProduct!!,
-            halfProduct.halfProductId,
-            weight,
-            chosenUnit
-        )
-        addHalfProductIncludedInDish(halfProductIncludedInDish)
-
-        return halfProduct
-    }
+  sealed class Result {
+    data class SuccessProduct(val name: String) : Result()
+    data class SuccessHalfProduct(val name: String) : Result()
+    object FailureWeight : Result()
+    object FailureDish : Result()
+    object FailureProduct : Result()
+  }
 }
--- app/src/main/java/com/erdees/foodcostcalc/ui/fragments/halfProductsFragment/addProductToHalfProductDialogFragment/AddProductToHalfProductFragment.kt
+++ app/src/main/java/com/erdees/foodcostcalc/ui/fragments/halfProductsFragment/addProductToHalfProductDialogFragment/AddProductToHalfProductFragment.kt
@@ -3,14 +3,16 @@ package com.erdees.foodcostcalc.ui.fragments.halfProductsFragment.addProductToHa
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
-import com.erdees.foodcostcalc.*
+import com.erdees.foodcostcalc.R
 import com.erdees.foodcostcalc.databinding.AddProductToHalfProductBinding
 import com.erdees.foodcostcalc.domain.model.halfProduct.HalfProductModel
 import com.erdees.foodcostcalc.ui.dialogFragments.informationDialogFragment.InformationDialogFragment
@@ -26,210 +28,175 @@ import com.erdees.foodcostcalc.utils.ViewUtils.showShortToast
 
 class AddProductToHalfProductFragment : DialogFragment(), AdapterView.OnItemSelectedListener {
 
-    private var _binding: AddProductToHalfProductBinding? = null
-    private val binding get() = _binding!!
+  private var _binding: AddProductToHalfProductBinding? = null
+  private val binding get() = _binding!!
 
-    private val unitList = arrayListOf<String>()
-    private lateinit var halfProductAdapter: ArrayAdapter<String>
-    private lateinit var productAdapter: ArrayAdapter<String>
-    private lateinit var unitAdapter: ArrayAdapter<String>
-    private lateinit var halfProductsList: MutableList<String>
-    private lateinit var productList: MutableList<String>
+  private val unitList = arrayListOf<String>()
+  private lateinit var halfProductAdapter: ArrayAdapter<String>
+  private lateinit var productsAdapter: ArrayAdapter<String>
+  private lateinit var unitAdapter: ArrayAdapter<String>
 
-    lateinit var viewModel: AddProductToHalfProductFragmentViewModel
+  lateinit var viewModel: AddProductToHalfProductFragmentViewModel
 
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    _binding = AddProductToHalfProductBinding.inflate(inflater, container, false)
+    val view = binding.root
+    viewModel =
+      ViewModelProvider(this)[AddProductToHalfProductFragmentViewModel::class.java]
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = AddProductToHalfProductBinding.inflate(inflater, container, false)
-        val view = binding.root
-        viewModel =
-            ViewModelProvider(this).get(AddProductToHalfProductFragmentViewModel::class.java)
+    binding.productWeightInHalfProduct.setOnFocusChangeListener { _, hasFocus ->
+      if (!hasFocus) view.hideKeyboard()
+    }
+    binding.weightForPiece.makeGone()
 
-        binding.productWeightInHalfProduct.setOnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) view.hideKeyboard()
-        }
-        binding.weightForPiece.makeGone()
+    viewModel.updateUnitsConditions()
 
-        viewModel.updateUnitsConditions()
-        setAdapters()
-        setHalfProductsSpinner()
-        setProductsSpinner()
+    viewModel.readAllHalfProductModelData.observe(viewLifecycleOwner) { halfProducts ->
+      halfProductAdapter =
+        ArrayAdapter(requireActivity(), R.layout.spinner_layout, halfProducts.map { it.name })
+      setHalfProductsSpinner()
+      halfProductAdapter.notifyDataSetChanged()
+      pickHalfProductIfPresent()
+    }
+
+    viewModel.readAllProductModelData.observe(viewLifecycleOwner) { products ->
+      Log.i(TAG,"readAllProductModelData $products")
+      productsAdapter =
+        ArrayAdapter(requireActivity(), R.layout.spinner_layout, products.map { it.name })
+      setProductsSpinner()
+      productsAdapter.notifyDataSetChanged()
+    }
+
+    binding.calculateWasteInfoButton.setOnClickListener {
+      InformationDialogFragment().show(this.parentFragmentManager, TAG)
+    }
+
+    binding.addProductToHalfproductBtn.setOnClickListener {
+      if (eitherOfSpinnersIsEmpty()) {
+        showShortToast(
+          requireContext(),
+          message = "You must pick half product and product."
+        )
+        return@setOnClickListener
+      } else if (binding.productWeightInHalfProduct.text.isNullOrEmpty() || binding.productWeightInHalfProduct.text.toString() == ".") {
+        showShortToast(requireContext(), message = "You can't add product without weight.")
+        return@setOnClickListener
+      } else if (!viewModel.isHalfProductPiece && viewModel.isProductPiece && binding.weightForPiece.text.isEmpty()) {
+        showShortToast(
+          requireContext(),
+          message = "You need to provide ${viewModel.getHalfProductUnitType()} of this product!"
+        )
+      } else {
+
+        val weight = binding.productWeightInHalfProduct.text.toString().toDouble()
+        val pieceWeight =
+          if (!viewModel.isHalfProductPiece && viewModel.isProductPiece) binding.weightForPiece.text.toString()
+            .toDouble() else 1.0
+        viewModel.addProductToHalfProduct(weight, pieceWeight)
+        binding.productWeightInHalfProduct.text.clear()
+        showShortToast(
+          requireContext(),
+          message = "${
+            viewModel.getChosenProductName()
+          } added."
+        )
+      }
+    }
+
+    dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    return view
+  }
+
+  private fun pickHalfProductIfPresent() {
+    val halfProductToSelect = passedHalfProduct ?: return
+    val positionToSelect = halfProductAdapter.getPosition(halfProductToSelect.name)
+    binding.halfProductSpinner.setSelection(positionToSelect)
+  }
+
+  private fun setUnitsSpinner() {
+    unitAdapter =
+      ArrayAdapter(requireActivity(), R.layout.support_simple_spinner_dropdown_item, unitList)
+    with(binding.unitSpinner) {
+      adapter = unitAdapter
+      onItemSelectedListener = this@AddProductToHalfProductFragment
+      prompt = getString(R.string.select_unit)
+      gravity = Gravity.CENTER
+      id = UNIT_SPINNER_ID
+      setSelection(0, false)
+    }
+  }
+
+  private fun setProductsSpinner() {
+    with(binding.productSpinner)
+    {
+      adapter = productsAdapter
+      onItemSelectedListener = this@AddProductToHalfProductFragment
+      prompt = getString(R.string.select_product)
+      gravity = Gravity.CENTER
+      id = PRODUCT_SPINNER_ID
+      setSelection(0, false)
+    }
+  }
+
+  private fun setHalfProductsSpinner() {
+    with(binding.halfProductSpinner) {
+      adapter = halfProductAdapter
+      id = HALFPRODUCT_SPINNER_ID
+      onItemSelectedListener = this@AddProductToHalfProductFragment
+      prompt = "choose half product"
+      gravity = Gravity.CENTER
+    }
+  }
+
+  companion object {
+    fun newInstance(): AddProductToHalfProductFragment =
+      AddProductToHalfProductFragment()
+
+    var passedHalfProduct: HalfProductModel? = null
+    const val TAG = "AddProductToHalfProductFragment"
+  }
+
+  private fun eitherOfSpinnersIsEmpty(): Boolean {
+    return (viewModel.readAllProductModelData.value.isNullOrEmpty() || viewModel.readAllHalfProductModelData.value.isNullOrEmpty())
+  }
+
+  private fun setTextField() {
+    if (viewModel.isProductPiece && !viewModel.isHalfProductPiece) {
+      binding.weightForPiece.makeVisible()
+      binding.weightForPiece.hint =
+        "${viewModel.getChosenProductName()} ${getPerUnitAsDescription(viewModel.getHalfProductUnit())}."
+    } else binding.weightForPiece.makeGone()
+  }
+
+  override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    when (parent?.id) {
+      1 -> {
+        viewModel.updateChosenProductData(position)
+        unitList.changeUnitList(
+          viewModel.getUnitType(),
+          viewModel.metricCondition,
+          viewModel.usaCondition
+        )
         setUnitsSpinner()
+        unitAdapter.notifyDataSetChanged()
+        viewModel.setUnit(unitList.first())
+        setTextField()
+      }
 
-        viewModel.readAllHalfProductModelData.observe(
-            viewLifecycleOwner
-        ) {
-          it.forEach { halfProduct -> halfProductsList.add(halfProduct.name) }
-          pickHalfProductIfPresent()
-        }
+      2 -> {
+        viewModel.updateChosenHalfProductData(position)
+        setTextField()
+      }
 
-      viewModel.readAllProductModelData.observe(
-            viewLifecycleOwner
-        ) { it.forEach { product -> productList.add(product.name) } }
-
-      viewModel.readAllProductModelData.observe(
-            viewLifecycleOwner
-        ) { products ->
-          productAdapter.clear()
-          products.forEach { product ->
-            productAdapter.add(product.name)
-            productAdapter.notifyDataSetChanged()
-          }
-        }
-
-      viewModel.readAllHalfProductModelData.observe(
-            viewLifecycleOwner
-        ) { halfProducts ->
-          halfProductAdapter.clear()
-          halfProducts.forEach { halfProduct ->
-            halfProductAdapter.add(halfProduct.name)
-            halfProductAdapter.notifyDataSetChanged()
-          }
-        }
-
-      binding.calculateWasteInfoButton.setOnClickListener {
-            InformationDialogFragment().show(
-                this.parentFragmentManager,
-                TAG
-            )
-        }
-        binding.addProductToHalfproductBtn.setOnClickListener {
-            if (eitherOfSpinnersIsEmpty()) {
-                showShortToast(
-                    requireContext(),
-                    message = "You must pick half product and product."
-                )
-                return@setOnClickListener
-            } else if (binding.productWeightInHalfProduct.text.isNullOrEmpty() || binding.productWeightInHalfProduct.text.toString() == ".") {
-                showShortToast(requireContext(), message = "You can't add product without weight.")
-                return@setOnClickListener
-            } else if (!viewModel.isHalfProductPiece && viewModel.isProductPiece && binding.weightForPiece.text.isEmpty()) {
-                showShortToast(
-                    requireContext(),
-                    message = "You need to provide ${viewModel.getHalfProductUnitType()} of this product!"
-                )
-            } else {
-
-                val weight = binding.productWeightInHalfProduct.text.toString().toDouble()
-                val pieceWeight =
-                    if (!viewModel.isHalfProductPiece && viewModel.isProductPiece) binding.weightForPiece.text.toString()
-                        .toDouble() else 1.0
-                viewModel.addProductToHalfProduct(weight, pieceWeight)
-                binding.productWeightInHalfProduct.text.clear()
-                showShortToast(
-                    requireContext(),
-                    message = "${
-                        viewModel.getChosenProductName()
-                    } added."
-                )
-            }
-        }
-
-        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        return view
+      else -> {
+        viewModel.setUnit(unitList[position])
+      }
     }
+  }
 
-    private fun pickHalfProductIfPresent() {
-        val halfProductToSelect = passedHalfProduct ?: return
-        val positionToSelect = halfProductsList.indexOf(halfProductToSelect.name)
-        binding.halfProductSpinner.setSelection(positionToSelect)
-    }
-
-    private fun setAdapters() {
-        halfProductsList = mutableListOf()
-        halfProductAdapter =
-            ArrayAdapter(requireActivity(), R.layout.spinner_layout, halfProductsList)
-        productList = mutableListOf()
-        productAdapter = ArrayAdapter(requireActivity(), R.layout.spinner_layout, productList)
-        unitAdapter =
-            ArrayAdapter(requireActivity(), R.layout.support_simple_spinner_dropdown_item, unitList)
-        unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-    }
-
-    private fun setUnitsSpinner() {
-        with(binding.unitSpinner) {
-            adapter = unitAdapter
-            setSelection(0, false)
-            onItemSelectedListener = this@AddProductToHalfProductFragment
-            prompt = "Select unit"
-            gravity = Gravity.CENTER
-            id = UNIT_SPINNER_ID
-        }
-    }
-
-    private fun setProductsSpinner() {
-        with(binding.productSpinner)
-        {
-            adapter = productAdapter
-            setSelection(0, false)
-            onItemSelectedListener = this@AddProductToHalfProductFragment
-            prompt = "Select product"
-            gravity = Gravity.CENTER
-            id = PRODUCT_SPINNER_ID
-        }
-        productAdapter.notifyDataSetChanged()
-    }
-
-    private fun setHalfProductsSpinner() {
-        with(binding.halfProductSpinner) {
-            adapter = halfProductAdapter
-            id = HALFPRODUCT_SPINNER_ID
-            onItemSelectedListener = this@AddProductToHalfProductFragment
-            prompt = "choose half product"
-            gravity = Gravity.CENTER
-        }
-        halfProductAdapter.notifyDataSetChanged()
-    }
-
-    companion object {
-        fun newInstance(): AddProductToHalfProductFragment =
-            AddProductToHalfProductFragment()
-        var passedHalfProduct : HalfProductModel? = null
-        const val TAG = "AddProductToHalfProductFragment"
-    }
-
-    private fun eitherOfSpinnersIsEmpty(): Boolean {
-        return (viewModel.readAllProductModelData.value.isNullOrEmpty() || viewModel.readAllHalfProductModelData.value.isNullOrEmpty())
-    }
-
-    private fun setTextField() {
-        if (viewModel.isProductPiece && !viewModel.isHalfProductPiece) {
-            binding.weightForPiece.makeVisible()
-            binding.weightForPiece.hint =
-                "${viewModel.getChosenProductName()} ${getPerUnitAsDescription(viewModel.getHalfProductUnit())}."
-        } else binding.weightForPiece.makeGone()
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        when (parent?.id) {
-            1 -> {
-                viewModel.updateChosenProductData(position)
-                unitList.changeUnitList(
-                    viewModel.getUnitType(),
-                    viewModel.metricCondition,
-                    viewModel.usaCondition
-                )
-                unitAdapter.notifyDataSetChanged()
-                binding.unitSpinner.setSelection(0, false)
-                viewModel.setUnit(unitList.first())
-                binding.unitSpinner.setSelection(0) // when the product is chosen first units got chosen immediately
-                setTextField()
-            }
-            2 -> {
-                viewModel.updateChosenHalfProductData(position)
-                setTextField()
-            }
-            else -> {
-                viewModel.setUnit(unitList[position])
-            }
-        }
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        this.dismiss()
-    }
+  override fun onNothingSelected(parent: AdapterView<*>?) {}
 }
