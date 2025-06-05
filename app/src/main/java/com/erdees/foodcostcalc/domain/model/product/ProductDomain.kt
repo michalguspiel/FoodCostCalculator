8d02b58e9c2cd722bc8ed23d1ad37944c6235430
michal <guspielmichal@gmail.com>
2025-06-05 16:41:05 +0300

    Remove tostring override

--- app/src/main/java/com/erdees/foodcostcalc/domain/model/product/ProductDomain.kt
+++ app/src/main/java/com/erdees/foodcostcalc/domain/model/product/ProductDomain.kt
@@ -7,20 +7,17 @@ import kotlinx.serialization.Serializable
 @Keep
 @Serializable
 data class ProductDomain(
-  override val id: Long,
-  override val name: String,
-  val pricePerUnit: Double,
-  val tax: Double,
-  val waste: Double,
-  val unit: String,
+    override val id: Long,
+    override val name: String,
+    val pricePerUnit: Double,
+    val tax: Double,
+    val waste: Double,
+    val unit: String,
 ) : Item {
 
-  private val priceWithTax = pricePerUnit * (1 + tax / 100)
+    private val priceWithTax = pricePerUnit * (1 + tax / 100)
 
-  val priceAfterWasteAndTax =
-    priceWithTax / (1 - waste / 100)
+    val priceAfterWasteAndTax =
+        priceWithTax / (1 - waste / 100)
 
-  override fun toString(): String {
-    return "$name, price $unit netto: $pricePerUnit.\nPrice $unit with foodcost: $priceAfterWasteAndTax."
-  }
 }
